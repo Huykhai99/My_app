1eadd427b96570fdce1d834d698ab1f52e41efc9
Huykhai99 <lehuykhai99@gmail.com>
2025-08-04 08:05:16 +0700

    Initial commit

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,8 +3,9 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -54,30 +55,25 @@ try {
     `;
   } catch (error) {
     // We'll log the error to the console for now
-    console.error(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    }
   }
   
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 
 }
-export async function updateInvoice(id: string,prevState: State,
- formData: FormData) {
-  const validatedFields = UpdateInvoice.safeParse({
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Update Invoice.',
-    };
-  }
-  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-try {
+ 
+  try {
     await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
@@ -87,6 +83,7 @@ try {
     // We'll log the error to the console for now
     console.error(error);
   }
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -95,4 +92,27 @@ export async function deleteInvoice(id: string) {
   
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', {
+      redirect: false, // Tắt redirect mặc định để tự xử lý
+      email: formData.get('email'),
+      password: formData.get('password'),
+      redirectTo: formData.get('redirectTo')?.toString() || '/dashboard',
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
--- nextjs-dashboard/app/ui/login-form.tsx
+++ nextjs-dashboard/app/ui/login-form.tsx
@@ -1,3 +1,4 @@
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -5,9 +6,10 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-
+import { Button } from '@/app/ui/button';
+ 
 export default function LoginForm() {
+
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -55,11 +57,11 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
         </div>
       </div>
     </form>
